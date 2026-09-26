c24a66eb24c300d76375728d89a392a56300170c
dhruvpratap4545-star <dhruvpratap4545@gmail.com>
2026-09-26 11:40:50 +0530

    Update Flask app to render index.html and add comments

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, Response
+from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
+# 1. मुख्य पब्लिक वेबसाइट (आपकी पुरानी साइट)
 @app.route('/')
 def home():
-    return "Dhruv Academy Server is Live!"
+    return render_template('index.html')
 
+# 2. एक्सोटेल का अलग वेबहुक
 @app.route('/voice-webhook', methods=['GET', 'POST'])
 def voice_webhook():
-    # एक्सोटेल के लिए सही XML रिस्पॉन्स फॉर्मेट
     xml_response = """<?xml version="1.0" encoding="UTF-8"?>
     <Response>
         <Say language="hi-IN">ध्रुव एकेडमी में आपका स्वागत है।</Say>
     </Response>"""
-    
     return Response(xml_response, mimetype='text/xml')
 
 if __name__ == '__main__':
